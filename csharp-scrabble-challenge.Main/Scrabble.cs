05abbc538fd93bd82569755c0b7d1cc85a869525
Jakub Mroz <kuba.mroz01@gmail.com>
2025-08-06 11:37:38 +0200

    feat: scrabble base case

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,61 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private Dictionary<char, int> letterToScoreMap = new()
+        {
+            {'a', 1},
+            {'b', 3},
+            {'c', 3},
+            {'d', 2},
+            {'e', 1},
+            {'f', 4},
+            {'g', 2},
+            {'h', 4},
+            {'i', 1},
+            {'j', 8},
+            {'k', 5},
+            {'l', 1},
+            {'m', 3},
+            {'n', 1},
+            {'o', 1},
+            {'p', 3},
+            {'q', 10},
+            {'r', 1},
+            {'s', 1},
+            {'t', 1},
+            {'u', 1},
+            {'v', 4},
+            {'w', 4},
+            {'x', 8},
+            {'y', 4},
+            {'z', 10}
+        };
+
+        private string processedWord;
+
         public Scrabble(string word)
         {            
             //TODO: do something with the word variable
+
+            bool isValid = word.All(char.IsLetter);
+
+
+            processedWord = word.ToLower();
+
         }
 
         public int score()
         {
+            int totalScore = 0;
+
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            foreach (var letter in processedWord)
+            {
+                letterToScoreMap.TryGetValue(letter, out var score);
+                totalScore += score;
+            }
+
+            return totalScore;
         }
     }
 }
